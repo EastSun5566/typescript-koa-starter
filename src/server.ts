4e88ebbe93440433c777c597e5791e96aa9c093f
汪東陽 <michael19920327@gmail.com>
2021-03-21 23:19:39 +0800

    chore: add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { Server } from 'http';
 import Koa from 'koa';
 import Router, { IRouterOptions } from 'koa-router';
@@ -14,18 +15,34 @@ interface ServerOptions extends IRouterOptions {
 export const createServer = (options: ServerOptions = {}): Server => {
   const router = createRouter(new Router(options));
 
-  const port = options.port || process.env.PORT || 8080;
-
-  const server = new Koa()
+  const app = new Koa()
     .use(helmet())
     .use(bodyParser())
     .use(errorHandler())
     .use(router.routes())
-    .use(router.allowedMethods())
-    .listen(port);
+    .use(router.allowedMethods());
 
-  // eslint-disable-next-line no-console
-  console.info(`[HTTP] listening on http://localhost:${port}${options.prefix || ''}`);
+  const port = options.port || process.env.PORT || 8080;
+  const server = app.listen(port, () => {
+    console.info(`[HTTP] Listening on http://localhost:${port}${options.prefix || ''}`);
+  });
+
+  process.on('uncaughtException', (err) => {
+    console.error('UncaughtException', err);
+    process.exit(1);
+  });
+
+  const handleSignals = (signals: NodeJS.Signals) => {
+    console.error(`Exit process in responding to ${signals}`);
+
+    console.info('Starting graceful shutdown');
+    server.close();
+
+    process.exit(0);
+  };
+
+  process.on('SIGTERM', handleSignals);
+  process.on('SIGINT', handleSignals);
 
   return server;
 };
