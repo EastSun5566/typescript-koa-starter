630ba4ed8d55aa1b8c692b40e39884f8b40fbf11
汪東陽 <michael19920327@gmail.com>
2021-04-01 17:40:54 +0800

    chore: extract register process event function

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 /* eslint-disable no-console */
 import { Server } from 'http';
 import Koa from 'koa';
-import Router, { IRouterOptions } from 'koa-router';
+import Router, { IRouterOptions as RouterOptions } from 'koa-router';
 import helmet from 'koa-helmet';
 import bodyParser from 'koa-bodyparser';
 
 import { createRouter } from './router';
 import { errorHandler } from './middlewares';
+import { registerProcessEvents } from './utils';
 
-interface ServerOptions extends IRouterOptions {
+interface ServerOptions extends RouterOptions {
   port?: number;
 }
 
@@ -27,22 +28,7 @@ export const createServer = (options: ServerOptions = {}): Server => {
     console.info(`[HTTP] Listening on http://localhost:${port}${options.prefix || ''}`);
   });
 
-  process.on('uncaughtException', (err) => {
-    console.error('UncaughtException', err);
-    process.exit(1);
-  });
-
-  const handleSignals = (signals: NodeJS.Signals) => {
-    console.error(`Exit process in responding to ${signals}`);
-
-    console.info('Starting graceful shutdown');
-    server.close();
-
-    process.exit(0);
-  };
-
-  process.on('SIGTERM', handleSignals);
-  process.on('SIGINT', handleSignals);
+  registerProcessEvents(server);
 
   return server;
 };
